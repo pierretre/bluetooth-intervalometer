34801a9541068aaffdc59b98b32a8ba04760cb1a
Pierre treton <pierre.treton3@gmail.com>
2021-01-23 17:58:52 +0100

    arduino code finished and bluetooth connection working on android app

--- app/app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/app/src/main/java/com/example/appandroid/MainActivity.java
@@ -1,73 +1,223 @@
 package com.example.appandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.location.LocationManagerCompat;
+
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 //import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Set;
 import java.util.UUID;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     static final UUID btUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
-    private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-
+    private BluetoothAdapter btAdapter;
+    private BluetoothDevice hc05_device;
+    private BluetoothCommunication bluetoothComThread;
 
     @Override
+    /**
+     *
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // the button that begins the take of photos 
+        Button intervalBtn= (Button) findViewById(R.id.intervalBtn);
+        intervalBtn.setText("begin photo shoot");
 
-        connectBT();
+        intervalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch(String.valueOf(intervalBtn.getText())){
+                    case "begin photo shoot" :
+                        Log.e("Status","begin");
+                        bluetoothComThread.send("STATUS");
+                        break;
+                    case "stop photo shoot" :
+                        Log.e("Status","stop");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
 
 
+        hc05_device=null;
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
 
+        if (pairedDevices.size() != 0){
+            for (BluetoothDevice device : pairedDevices) {
+                if(device.getName().equals("HC-05"))
+                    hc05_device=device;
+            }
+        }
 
-
-
+        // if hc05 was not found in the list of paired devices :
+        if(hc05_device==null){
+            return;
+        }
+        BluetoothConnectThread btct=new BluetoothConnectThread(hc05_device);
+        btct.start();
     }
 
-    private void connectBT(){
-        try {
-            enableBT();
-            BluetoothDevice intervalometer_hc05 = btAdapter.getRemoteDevice("98:D3:21:F7:50:63");
-
-            BluetoothSocket btSocket = intervalometer_hc05.createInsecureRfcommSocketToServiceRecord(btUUID);
 
 
-            while(!btSocket.isConnected())
+    private class BluetoothConnectThread extends Thread{
+
+        private BluetoothSocket btSocket;
+
+        public BluetoothConnectThread(BluetoothDevice device){
+            BluetoothSocket tmp = null;
+            UUID uuid = device.getUuids()[0].getUuid();
+
+            try {
+                tmp = device.createInsecureRfcommSocketToServiceRecord(uuid);
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "exception", Toast.LENGTH_SHORT).show();
+            }
+            this.btSocket=tmp;
+        }
+
+        public void run(){
+            // try to make connection with bluetooth device
+
+            btAdapter.cancelDiscovery();
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
                 btSocket.connect();
-            Toast.makeText(getApplicationContext(), "connected to BT", Toast.LENGTH_LONG).show();
-
-        }catch (Exception e){
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Exception : couldn't connect to BT", Toast.LENGTH_LONG).show();
+                Log.e("Status", "Device connected");
+//                handler.obtainMessage(CONNECTING_STATUS, 1, -1).sendToTarget();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    btSocket.close();
+                    Log.e("Status", "Cannot connect to device");
+//                    handler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                } catch (IOException closeException) {
+                    Log.e("Status", "Could not close the client socket", closeException);
+                }
+                return;
+            }
+            bluetoothComThread=new BluetoothCommunication(this.btSocket);
+            bluetoothComThread.start();
         }
     }
 
-    private void enableBT(){
-        if(btAdapter == null){
-            Toast.makeText(getApplicationContext(), "can't find bluetooth adapter", Toast.LENGTH_SHORT).show();
-        }
-        if(!btAdapter.isEnabled()){
-            Toast.makeText(getApplicationContext(), "bluetooth not enabled", Toast.LENGTH_SHORT).show();
+    private static class BluetoothCommunication extends Thread {
 
-            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBT);
+        private InputStream input;
+        private OutputStream output;
+        private BluetoothSocket socket;
+
+        public BluetoothCommunication(BluetoothSocket socket){
+            this.socket=socket;
+
+            try{
+                this.input=socket.getInputStream();
+                this.output=socket.getOutputStream();
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+            }
         }
-        if(btAdapter.isEnabled()){
-            Toast.makeText(getApplicationContext(), "bluetooth enabled, it's all good", Toast.LENGTH_SHORT).show();
+
+        public void run(){
+
+            send("STATUS");
+
+            byte[] messageIn=new byte[1024];
+            String inputString=null;
+            int byteIndex=0;
+            boolean isConnected=true;
+            String[] message;
+
+            // Listens to the input of the bluetooth socket to read incoming messages
+            while(isConnected){
+                try{
+                    Log.e("Status","Listening on socket input");
+
+                    messageIn[byteIndex] = (byte) this.input.read();
+                    if (messageIn[byteIndex] == '\n'){
+                        inputString = new String(messageIn,0,byteIndex);
+                        Log.e("Arduino Message",inputString);
+//                        handler.obtainMessage(MESSAGE_READ,readMessage).sendToTarget();
+                        byteIndex = 0;
+                    } else {
+                        byteIndex++;
+                    }
+
+                    // if the message was received entirely
+                    if(byteIndex==0){
+                        message=inputString.trim().split("|");
+
+                        if(message[0]=="STATUS"){
+                            if(message[1]=="RUNNING") {
+                                // Sets the state of the app to running
+                                // if message[2]!=null:
+                                // OK : prints the message (message[2]) coming from arduino
+
+                                if(message[2]!=null)
+                                    Log.e("STATUS = RUNNING ",message[2]);
+                                else
+                                    Log.e("STATUS = RUNNING ","no message");
+
+                            }else if(message[1]=="WAITING") {
+                                // Sets the state of the app to waiting
+                                // if message[2]!=null:
+                                // ERROR : prints the error message (message[2]) coming from arduino
+
+                                if(message[2]!=null)
+                                    Log.e("STATUS = WAITING ",message[2]);
+                                else
+                                    Log.e("STATUS = WAITING ","no error");
+                            }
+                        }else {
+
+                        }
+                    }
+                }catch (IOException ioe){
+                    ioe.printStackTrace();
+                    isConnected=false;
+                }
+            }
+        }
+
+        public void send(String command){
+            try{
+                byte[] bytes = command.getBytes();
+                this.output.write(bytes);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+            }
         }
     }
+
+
 }
--- bt-intervalometer/bt-intervalometer.ino
+++ bt-intervalometer/bt-intervalometer.ino
@@ -13,7 +13,7 @@ void setup() {
 }
 
 void loop() {
-  boolean OK_values = true;
+  boolean OK_values = false;
 
   while (mySerial.available() == 0);
 
@@ -22,35 +22,38 @@ void loop() {
 
   inputString.trim();
 
-  if (inputString == "TAKE_SINGLE_SHOT") {
+  if (inputString == "STATUS") {
+    mySerial.write("STATUS|WAITING\n");
+  } else if (inputString == "TAKE_SINGLE_SHOT") {
     // take a single picture
     //digitalWrite(CAMERA, LOW);
     delay(250);
     //digitalWrite(CAMERA, HIGH);
 
-  } else {
+  } else if (inputString.substring(4, 10) == "RUN") {
     shutter_speed = inputString.substring(4, 10).toFloat();
     time_between_pics = inputString.substring(11, 17).toFloat();
     pics_number = inputString.substring(18, 22).toInt();
 
+    OK_values=true;
+
     if (shutter_speed <= 0 || shutter_speed > 35) {
       //error message to tell him that he must fill in a positive number below 35
-      mySerial.write("ERROR : you must specify a shutter speed between 0.001 and 35 \n");
+      mySerial.write("STATUS|WAITING|You must specify a shutter speed between 0.001 and 35 \n");
       OK_values = false;
     }
     if (time_between_pics <= ((int) 1.5 * shutter_speed)) {
       //error message to tell him that he must fill in a number at least equal to 1.5 times the shutter speed
-      mySerial.write("ERROR : you must specify more time between pictures for your camera to process safely \n");
+      mySerial.write("STATUS|WAITING|You must specify more time between pictures for your camera to process safely \n");
       OK_values = false;
     }
     if (pics_number <= 0) {
       //error message to tell him that he can't take 0 or less pictures
-      mySerial.write("ERROR : you must take at least on picture \n");
+      mySerial.write("STATUS|WAITING|You must take at least on picture \n");
       OK_values = false;
     }
   }
   if (OK_values) {
-
     float delay_duration = shutter_speed + time_between_pics;
     int seconds = (int)(delay_duration * pics_number);
 
@@ -58,34 +61,39 @@ void loop() {
     int minutes = (seconds - s) / 60;
     int hours = (minutes - (minutes % 60)) / 60;
 
-    mySerial.println("Take a photo every : " + String(delay_duration) + "s\n");
-    mySerial.println("Total number of pictures : " + String(pics_number) + "\n");
-    mySerial.println("Total time : " + String(hours) + "h" + (minutes % 60) + "m" + (s + 1) + "s\n");
+    //mySerial.println("Take a photo every : " + String(delay_duration) + "s\n");
+    //mySerial.println("Total number of pictures : " + String(pics_number) + "\n");
+    //mySerial.println("Total time : " + String(hours) + "h" + (minutes % 60) + "m" + (s + 1) + "s\n");
 
+    mySerial.print("STATUS|RUNNING|Take a photo every : " + String(delay_duration) + "s:Total number of pictures : " + String(pics_number) +":Total time : " + String(hours) + "h" + String(minutes % 60) + "m" + String(s + 1) + "s\n");
+    
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // take pictures while the number of pics taken does not exceed the number of pics filled in by the user //
+    // take pictures while the number of pics taken does not exceed the number of pics filled in by the user
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     while (pics_number > 0) {
-      if (mySerial.available() != 0) {
-        String inString = mySerial.readString();
-        Serial.print("Message received : "); Serial.println(inString);
 
-        inString.trim();
+        if (mySerial.available() != 0) {
+          String inString = mySerial.readString();
+          Serial.print("Message received : "); Serial.println(inString);
 
-        if (inputString == "STOP") {
-          pics_number = 0;
-          mySerial.write("STOPPED !! \n");
+          inString.trim();
+
+          if (inString == "STATUS")
+            mySerial.write("STATUS|RUNNING\n");
+          else if (inString == "STOP")
+            pics_number = 0;
+
+        } else {
+          //digitalWrite(2, LOW);
+          delay(250);
+          //digitalWrite(2, HIGH);
+          delay(delay_duration * 1000 - 250);
+
+          Serial.print("i");
+          pics_number--;
         }
-      } else {
-        //digitalWrite(2, LOW);
-        delay(250);
-        //digitalWrite(2, HIGH);
-        delay(delay_duration * 1000 - 250);
-
-        Serial.print("i");
-        pics_number--;
       }
-    }
+    mySerial.write("STATUS|WAITING\n");
   }
 }
